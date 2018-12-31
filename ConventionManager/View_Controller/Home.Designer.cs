46438dfff0ed44a1dae9e69581e62347ba0f0e36
Elvin Shrestha <37188510+elwyncrestha@users.noreply.github.com>
2018-12-31 16:48:42 +0545

    MVC pattern and Database(SQL) configured

---
+++ ConventionManager/View_Controller/Home.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ConventionManager
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.attendeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAttendeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayEditDeleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addEventToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayAllEventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addSeminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayEditDeleteSeminarsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addStallToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayEditDeleteStallsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.attendeeToolStripMenuItem,
+            this.eventToolStripMenuItem,
+            this.seminarToolStripMenuItem,
+            this.stallToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // attendeeToolStripMenuItem
+            // 
+            this.attendeeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addAttendeeToolStripMenuItem,
+            this.displayEditDeleteToolStripMenuItem});
+            this.attendeeToolStripMenuItem.Name = "attendeeToolStripMenuItem";
+            this.attendeeToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.attendeeToolStripMenuItem.Text = "Attendee";
+            // 
+            // addAttendeeToolStripMenuItem
+            // 
+            this.addAttendeeToolStripMenuItem.Name = "addAttendeeToolStripMenuItem";
+            this.addAttendeeToolStripMenuItem.Size = new System.Drawing.Size(231, 22);
+            this.addAttendeeToolStripMenuItem.Text = "Add Attendee";
+            // 
+            // displayEditDeleteToolStripMenuItem
+            // 
+            this.displayEditDeleteToolStripMenuItem.Name = "displayEditDeleteToolStripMenuItem";
+            this.displayEditDeleteToolStripMenuItem.Size = new System.Drawing.Size(231, 22);
+            this.displayEditDeleteToolStripMenuItem.Text = "Display/Edit/Delete Attendees";
+            // 
+            // eventToolStripMenuItem
+            // 
+            this.eventToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addEventToolStripMenuItem,
+            this.displayAllEventsToolStripMenuItem});
+            this.eventToolStripMenuItem.Name = "eventToolStripMenuItem";
+            this.eventToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.eventToolStripMenuItem.Text = "Event";
+            // 
+            // addEventToolStripMenuItem
+            // 
+            this.addEventToolStripMenuItem.Name = "addEventToolStripMenuItem";
+            this.addEventToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.addEventToolStripMenuItem.Text = "Add Event";
+            // 
+            // displayAllEventsToolStripMenuItem
+            // 
+            this.displayAllEventsToolStripMenuItem.Name = "displayAllEventsToolStripMenuItem";
+            this.displayAllEventsToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.displayAllEventsToolStripMenuItem.Text = "Display/Edit/Delete Events";
+            // 
+            // seminarToolStripMenuItem
+            // 
+            this.seminarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addSeminarToolStripMenuItem,
+            this.displayEditDeleteSeminarsToolStripMenuItem});
+            this.seminarToolStripMenuItem.Name = "seminarToolStripMenuItem";
+            this.seminarToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.seminarToolStripMenuItem.Text = "Seminar";
+            // 
+            // addSeminarToolStripMenuItem
+            // 
+            this.addSeminarToolStripMenuItem.Name = "addSeminarToolStripMenuItem";
+            this.addSeminarToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.addSeminarToolStripMenuItem.Text = "Add Seminar";
+            // 
+            // displayEditDeleteSeminarsToolStripMenuItem
+            // 
+            this.displayEditDeleteSeminarsToolStripMenuItem.Name = "displayEditDeleteSeminarsToolStripMenuItem";
+            this.displayEditDeleteSeminarsToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.displayEditDeleteSeminarsToolStripMenuItem.Text = "Display/Edit/Delete Seminars";
+            // 
+            // stallToolStripMenuItem
+            // 
+            this.stallToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addStallToolStripMenuItem,
+            this.displayEditDeleteStallsToolStripMenuItem});
+            this.stallToolStripMenuItem.Name = "stallToolStripMenuItem";
+            this.stallToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.stallToolStripMenuItem.Text = "Stall";
+            // 
+            // addStallToolStripMenuItem
+            // 
+            this.addStallToolStripMenuItem.Name = "addStallToolStripMenuItem";
+            this.addStallToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.addStallToolStripMenuItem.Text = "Add Stall";
+            // 
+            // displayEditDeleteStallsToolStripMenuItem
+            // 
+            this.displayEditDeleteStallsToolStripMenuItem.Name = "displayEditDeleteStallsToolStripMenuItem";
+            this.displayEditDeleteStallsToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.displayEditDeleteStallsToolStripMenuItem.Text = "Display/Edit/Delete Stalls";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Home";
+            this.Text = "Home";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem attendeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addAttendeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayEditDeleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addEventToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayAllEventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addSeminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayEditDeleteSeminarsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addStallToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayEditDeleteStallsToolStripMenuItem;
+    }
+}
---
+++ SQL.sql
@@ -0,0 +1,100 @@
+create database ConventionManager;
+use ConventionManager;
+
+--- Independent tables ---
+create table Attendee
+(
+	AttendeeId int not null identity,
+	AttendeeFName varchar(255) not null,
+	AttendeeLName varchar(255) not null,
+	AttendeeEmail varchar(255) not null,
+	AttendeeContact varchar(255) not null,
+	AttendeeAddress varchar(255) not null,
+	TicketType varchar(255) not null,
+	AttendingDays int not null,
+	IsAvailable bit not null,
+	constraint pk_Attendee primary key (AttendeeId),
+	constraint ix_AttendeeEmail unique (AttendeeEmail)
+);
+
+create table Room
+(
+	RoomId int not null identity,
+	RoomName varchar(255) not null,
+	RoomLocationCode varchar(255) not null,
+	RoomCapacity int not null,
+	RoomResources varchar(255) not null,
+	constraint pk_Room primary key (RoomId),
+	constraint ix_RoomName unique (RoomName)
+);
+
+create table Stall
+(
+	StallId int not null identity,
+	StallName varchar(255) not null,
+	StallLocationCode varchar(255) not null,
+	StallCapacity int not null,
+	StallResources varchar(255) not null,
+	StallStartDate datetime not null,
+	StallEndDate datetime not null,
+	StallType varchar(255) not null,
+	constraint pk_Stall primary key (StallId),
+	constraint ix_StallName unique (StallName)
+);
+
+--- One key dependent tables ---
+create table Event
+(
+	EventId int not null identity,
+	EventName varchar(255) not null,
+	EventStartDate datetime not null,
+	EventEndDate datetime not null,
+	EventDescription varchar(500) not null,
+	RoomId int not null,
+	constraint pk_Event primary key (EventId),
+	constraint fk_Event_Room foreign key (RoomId) references Room (RoomId)
+);
+
+create table Seminar
+(
+	SeminarId int not null identity,
+	SeminarName varchar(255) not null,
+	SeminarStartDate datetime not null,
+	SeminarEndDate datetime not null,
+	RoomId int not null,
+	constraint pk_Seminar primary key (SeminarId),
+	constraint fk_Seminar_Room foreign key (RoomId) references Room (RoomId)
+);
+
+--- Two keys dependent tables ---
+create table AttendeeStall
+(
+	Id int not null identity,
+	AttendeeId int not null,
+	StallId int not null,
+	constraint pk_AttendeeStall primary key (Id),
+	constraint fk_AttendeeStall_Attendee foreign key (AttendeeId) references Attendee(AttendeeId),
+	constraint fk_AttendeeStall_Stall foreign key (StallId) references Stall(StallId)	
+);
+
+create table AttendeeEvent
+(
+	Id int not null identity,
+	AttendeeId int not null,
+	EventId int not null,
+	constraint pk_AttendeeEvent primary key (Id),
+	constraint fk_AttendeeEvent_Attendee foreign key (AttendeeId) references Attendee(AttendeeId),
+	constraint fk_AttendeeEvent_Event foreign key (EventId) references Event(EventId)	
+);
+
+create table AttendeeSeminar
+(
+	Id int not null identity,
+	AttendeeId int not null,
+	SeminarId int not null,
+	constraint pk_AttendeeSeminar primary key (Id),
+	constraint fk_AttendeeSeminar_Attendee foreign key (AttendeeId) references Attendee(AttendeeId),
+	constraint fk_AttendeeSeminar_Event foreign key (SeminarId) references Seminar(SeminarId)	
+);
+
+SELECT Distinct TABLE_NAME FROM information_schema.TABLES;
