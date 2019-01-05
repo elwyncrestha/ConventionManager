f7db062863a026e9e6d894897e050499e10c4ada
Elvin Shrestha <37188510+elwyncrestha@users.noreply.github.com>
2019-01-05 15:59:44 +0545

    Update: UI

--- ConventionManager/View_Controller/Home.Designer.cs
+++ ConventionManager/View_Controller/Home.Designer.cs
@@ -45,7 +45,16 @@
             this.mstrManageRoom = new System.Windows.Forms.ToolStripMenuItem();
             this.mstrRoomUsage = new System.Windows.Forms.ToolStripMenuItem();
             this.mstrTEST = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbxConventionManager = new System.Windows.Forms.GroupBox();
+            this.lblIntro = new System.Windows.Forms.Label();
+            this.lblDetail1 = new System.Windows.Forms.Label();
+            this.lblDetail2 = new System.Windows.Forms.Label();
+            this.lblDetail3 = new System.Windows.Forms.Label();
+            this.lblDetail4 = new System.Windows.Forms.Label();
+            this.lblDetail5 = new System.Windows.Forms.Label();
+            this.lblDetail6 = new System.Windows.Forms.Label();
             this.stripHome.SuspendLayout();
+            this.gbxConventionManager.SuspendLayout();
             this.SuspendLayout();
             // 
             // attendeeToolStripMenuItem
@@ -53,13 +62,13 @@
             this.attendeeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mstrManageAttendee});
             this.attendeeToolStripMenuItem.Name = "attendeeToolStripMenuItem";
-            this.attendeeToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.attendeeToolStripMenuItem.Size = new System.Drawing.Size(89, 25);
             this.attendeeToolStripMenuItem.Text = "Attendee";
             // 
             // mstrManageAttendee
             // 
             this.mstrManageAttendee.Name = "mstrManageAttendee";
-            this.mstrManageAttendee.Size = new System.Drawing.Size(168, 22);
+            this.mstrManageAttendee.Size = new System.Drawing.Size(210, 26);
             this.mstrManageAttendee.Text = "Manage Attendee";
             this.mstrManageAttendee.Click += new System.EventHandler(this.mstrManageAttendee_Click);
             // 
@@ -69,20 +78,20 @@
             this.mstrManageEvent,
             this.mstrAddAttendeeToEvent});
             this.eventToolStripMenuItem.Name = "eventToolStripMenuItem";
-            this.eventToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.eventToolStripMenuItem.Size = new System.Drawing.Size(62, 25);
             this.eventToolStripMenuItem.Text = "Event";
             // 
             // mstrManageEvent
             // 
             this.mstrManageEvent.Name = "mstrManageEvent";
-            this.mstrManageEvent.Size = new System.Drawing.Size(193, 22);
+            this.mstrManageEvent.Size = new System.Drawing.Size(242, 26);
             this.mstrManageEvent.Text = "Manage Event";
             this.mstrManageEvent.Click += new System.EventHandler(this.mstrManageEvent_Click);
             // 
             // mstrAddAttendeeToEvent
             // 
             this.mstrAddAttendeeToEvent.Name = "mstrAddAttendeeToEvent";
-            this.mstrAddAttendeeToEvent.Size = new System.Drawing.Size(193, 22);
+            this.mstrAddAttendeeToEvent.Size = new System.Drawing.Size(242, 26);
             this.mstrAddAttendeeToEvent.Text = "Add Attendee to Event";
             this.mstrAddAttendeeToEvent.Click += new System.EventHandler(this.mstrAddAttendeeToEvent_Click);
             // 
@@ -92,20 +101,20 @@
             this.mstrManageSeminar,
             this.mstrAddAttendeeToSeminar});
             this.seminarToolStripMenuItem.Name = "seminarToolStripMenuItem";
-            this.seminarToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.seminarToolStripMenuItem.Size = new System.Drawing.Size(81, 25);
             this.seminarToolStripMenuItem.Text = "Seminar";
             // 
             // mstrManageSeminar
             // 
             this.mstrManageSeminar.Name = "mstrManageSeminar";
-            this.mstrManageSeminar.Size = new System.Drawing.Size(207, 22);
+            this.mstrManageSeminar.Size = new System.Drawing.Size(261, 26);
             this.mstrManageSeminar.Text = "Manage Seminar";
             this.mstrManageSeminar.Click += new System.EventHandler(this.mstrManageSeminar_Click);
             // 
             // mstrAddAttendeeToSeminar
             // 
             this.mstrAddAttendeeToSeminar.Name = "mstrAddAttendeeToSeminar";
-            this.mstrAddAttendeeToSeminar.Size = new System.Drawing.Size(207, 22);
+            this.mstrAddAttendeeToSeminar.Size = new System.Drawing.Size(261, 26);
             this.mstrAddAttendeeToSeminar.Text = "Add Attendee to Seminar";
             this.mstrAddAttendeeToSeminar.Click += new System.EventHandler(this.mstrAddAttendeeToSeminar_Click);
             // 
@@ -115,26 +124,27 @@
             this.mstrManageStall,
             this.mstrAddAttendeeToStall});
             this.stallToolStripMenuItem.Name = "stallToolStripMenuItem";
-            this.stallToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.stallToolStripMenuItem.Size = new System.Drawing.Size(54, 25);
             this.stallToolStripMenuItem.Text = "Stall";
             // 
             // mstrManageStall
             // 
             this.mstrManageStall.Name = "mstrManageStall";
-            this.mstrManageStall.Size = new System.Drawing.Size(186, 22);
+            this.mstrManageStall.Size = new System.Drawing.Size(234, 26);
             this.mstrManageStall.Text = "Manage Stall";
             this.mstrManageStall.Click += new System.EventHandler(this.mstrManageStall_Click);
             // 
             // mstrAddAttendeeToStall
             // 
             this.mstrAddAttendeeToStall.Name = "mstrAddAttendeeToStall";
-            this.mstrAddAttendeeToStall.Size = new System.Drawing.Size(186, 22);
+            this.mstrAddAttendeeToStall.Size = new System.Drawing.Size(234, 26);
             this.mstrAddAttendeeToStall.Text = "Add Attendee to Stall";
             this.mstrAddAttendeeToStall.Click += new System.EventHandler(this.mstrAddAttendeeToStall_Click);
             // 
             // stripHome
             // 
             this.stripHome.BackColor = System.Drawing.Color.Transparent;
+            this.stripHome.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.stripHome.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.attendeeToolStripMenuItem,
             this.eventToolStripMenuItem,
@@ -144,7 +154,7 @@
             this.mstrTEST});
             this.stripHome.Location = new System.Drawing.Point(0, 0);
             this.stripHome.Name = "stripHome";
-            this.stripHome.Size = new System.Drawing.Size(402, 24);
+            this.stripHome.Size = new System.Drawing.Size(438, 29);
             this.stripHome.TabIndex = 0;
             this.stripHome.Text = "menuStrip1";
             // 
@@ -154,29 +164,110 @@
             this.mstrManageRoom,
             this.mstrRoomUsage});
             this.roomToolStripMenuItem.Name = "roomToolStripMenuItem";
-            this.roomToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.roomToolStripMenuItem.Size = new System.Drawing.Size(64, 25);
             this.roomToolStripMenuItem.Text = "Room";
             // 
             // mstrManageRoom
             // 
             this.mstrManageRoom.Name = "mstrManageRoom";
-            this.mstrManageRoom.Size = new System.Drawing.Size(152, 22);
+            this.mstrManageRoom.Size = new System.Drawing.Size(185, 26);
             this.mstrManageRoom.Text = "Manage Room";
             this.mstrManageRoom.Click += new System.EventHandler(this.mstrManageRoom_Click);
             // 
             // mstrRoomUsage
             // 
             this.mstrRoomUsage.Name = "mstrRoomUsage";
-            this.mstrRoomUsage.Size = new System.Drawing.Size(152, 22);
+            this.mstrRoomUsage.Size = new System.Drawing.Size(185, 26);
             this.mstrRoomUsage.Text = "Room Usage";
             this.mstrRoomUsage.Click += new System.EventHandler(this.mstrRoomUsage_Click);
             // 
             // mstrTEST
             // 
             this.mstrTEST.Name = "mstrTEST";
-            this.mstrTEST.Size = new System.Drawing.Size(45, 20);
+            this.mstrTEST.Size = new System.Drawing.Size(56, 25);
             this.mstrTEST.Text = "TEST";
             this.mstrTEST.Click += new System.EventHandler(this.mstrTEST_Click);
+            // 
+            // gbxConventionManager
+            // 
+            this.gbxConventionManager.BackColor = System.Drawing.Color.Transparent;
+            this.gbxConventionManager.Controls.Add(this.lblDetail6);
+            this.gbxConventionManager.Controls.Add(this.lblDetail5);
+            this.gbxConventionManager.Controls.Add(this.lblDetail4);
+            this.gbxConventionManager.Controls.Add(this.lblDetail3);
+            this.gbxConventionManager.Controls.Add(this.lblDetail2);
+            this.gbxConventionManager.Controls.Add(this.lblDetail1);
+            this.gbxConventionManager.Controls.Add(this.lblIntro);
+            this.gbxConventionManager.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F);
+            this.gbxConventionManager.Location = new System.Drawing.Point(13, 59);
+            this.gbxConventionManager.Name = "gbxConventionManager";
+            this.gbxConventionManager.Size = new System.Drawing.Size(413, 241);
+            this.gbxConventionManager.TabIndex = 1;
+            this.gbxConventionManager.TabStop = false;
+            this.gbxConventionManager.Text = "Convention Manager System";
+            // 
+            // lblIntro
+            // 
+            this.lblIntro.AutoSize = true;
+            this.lblIntro.Location = new System.Drawing.Point(6, 44);
+            this.lblIntro.Name = "lblIntro";
+            this.lblIntro.Size = new System.Drawing.Size(381, 21);
+            this.lblIntro.TabIndex = 2;
+            this.lblIntro.Text = "A multi-feature system to well manage conventions.";
+            // 
+            // lblDetail1
+            // 
+            this.lblDetail1.AutoSize = true;
+            this.lblDetail1.Location = new System.Drawing.Point(6, 76);
+            this.lblDetail1.Name = "lblDetail1";
+            this.lblDetail1.Size = new System.Drawing.Size(381, 21);
+            this.lblDetail1.TabIndex = 3;
+            this.lblDetail1.Text = "This system tracks and manage following elements:";
+            // 
+            // lblDetail2
+            // 
+            this.lblDetail2.AutoSize = true;
+            this.lblDetail2.Location = new System.Drawing.Point(6, 122);
+            this.lblDetail2.Name = "lblDetail2";
+            this.lblDetail2.Size = new System.Drawing.Size(209, 21);
+            this.lblDetail2.TabIndex = 4;
+            this.lblDetail2.Text = "- Rooms and Stall Locations";
+            // 
+            // lblDetail3
+            // 
+            this.lblDetail3.AutoSize = true;
+            this.lblDetail3.Location = new System.Drawing.Point(6, 143);
+            this.lblDetail3.Name = "lblDetail3";
+            this.lblDetail3.Size = new System.Drawing.Size(93, 21);
+            this.lblDetail3.TabIndex = 5;
+            this.lblDetail3.Text = "- Attendees";
+            // 
+            // lblDetail4
+            // 
+            this.lblDetail4.AutoSize = true;
+            this.lblDetail4.Location = new System.Drawing.Point(6, 164);
+            this.lblDetail4.Name = "lblDetail4";
+            this.lblDetail4.Size = new System.Drawing.Size(58, 21);
+            this.lblDetail4.TabIndex = 6;
+            this.lblDetail4.Text = "- Stalls";
+            // 
+            // lblDetail5
+            // 
+            this.lblDetail5.AutoSize = true;
+            this.lblDetail5.Location = new System.Drawing.Point(6, 185);
+            this.lblDetail5.Name = "lblDetail5";
+            this.lblDetail5.Size = new System.Drawing.Size(85, 21);
+            this.lblDetail5.TabIndex = 7;
+            this.lblDetail5.Text = "- Seminars";
+            // 
+            // lblDetail6
+            // 
+            this.lblDetail6.AutoSize = true;
+            this.lblDetail6.Location = new System.Drawing.Point(6, 206);
+            this.lblDetail6.Name = "lblDetail6";
+            this.lblDetail6.Size = new System.Drawing.Size(66, 21);
+            this.lblDetail6.TabIndex = 8;
+            this.lblDetail6.Text = "- Events";
             // 
             // Home
             // 
@@ -184,7 +275,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ConventionManager.Properties.Resources.background1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(402, 312);
+            this.ClientSize = new System.Drawing.Size(438, 312);
+            this.Controls.Add(this.gbxConventionManager);
             this.Controls.Add(this.stripHome);
             this.Font = new System.Drawing.Font("Franklin Gothic Demi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -197,6 +289,8 @@
             this.Text = "Convention Manager Home";
             this.stripHome.ResumeLayout(false);
             this.stripHome.PerformLayout();
+            this.gbxConventionManager.ResumeLayout(false);
+            this.gbxConventionManager.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +314,13 @@
         private System.Windows.Forms.ToolStripMenuItem mstrAddAttendeeToSeminar;
         private System.Windows.Forms.ToolStripMenuItem mstrTEST;
         private System.Windows.Forms.ToolStripMenuItem mstrRoomUsage;
+        private System.Windows.Forms.GroupBox gbxConventionManager;
+        private System.Windows.Forms.Label lblDetail1;
+        private System.Windows.Forms.Label lblIntro;
+        private System.Windows.Forms.Label lblDetail6;
+        private System.Windows.Forms.Label lblDetail5;
+        private System.Windows.Forms.Label lblDetail4;
+        private System.Windows.Forms.Label lblDetail3;
+        private System.Windows.Forms.Label lblDetail2;
     }
 }
